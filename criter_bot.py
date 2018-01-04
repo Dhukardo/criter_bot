7e1549c707a323530c773c4da2052b2c5a2ac778
Dhukardo <35064138+Dhukardo@users.noreply.github.com>
2018-01-04 01:59:46 +0100

    Trasteando.
    Linea 14: He intentado probar que a la hora de contestar al comando /hola el Bot mencione al usuario por su nombre. ( TEST: No crashea pero aun no lo he probado)

--- criter_bot.py
+++ criter_bot.py
@@ -1,16 +1,23 @@
+#LLAMADA AL PROCESO
+
 import telegram
 from telegram.ext import Updater, CommandHandler
 
 token = "531793124:AAG4aWW484TmfQL8YrdhU_jfmM_fCBVs01M"
 
+#DEFINIENDO FUNCION DE MENSAJE PARA /hola
+
 def hola (bot,update):
 	msg = update.message.text
 	bot.send_message(
                 chat_id=update.message.chat_id,
-                text= "Ñiargh! Deja de molestar!"
-            )
+                text= "Ñiargh! Deja de molestar!".format(update.message.from_user.first_name))
+	
+	
 updater = Updater(token)
 dp = updater.dispatcher
+		
 start_handler = CommandHandler('hola', hola)
 dp.add_handler(start_handler)
+		
 updater.start_polling()
